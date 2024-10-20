f2480ca97f4752e5b8a76a5f4c4e54114fda02b2
wahini <vito.atmo@gmail.com>
2024-10-20 09:40:32 +0700

    Commit
    stable, dark de, keyboard, font bolded

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 /* ./src/components/Header/Header.js */
+
 import React from 'react';
 import './Header.css';
 
 const Header = ({ handleRefresh }) => (
-  <header className="top-header">
-    <h1>Welcome to Pajax</h1>
+  <header className="header">
+    <h1 className="header-title">Welcome to Pajax</h1>
   </header>
 );
 
