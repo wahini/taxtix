a824a1a91ce86dc66daadf4b72124130511c25bb
wahini <vito.atmo@gmail.com>
2024-10-20 23:17:43 +0700

    Commit
    change: icon T, seo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // Suggested Directory: ./src/App.js
 
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Header from './components/Header/Header';
 import WordleGrid from './components/WordleGrid/WordleGrid';
 import Footer from './components/Footer/Footer';
@@ -18,6 +19,15 @@ function App() {
 
   return (
     <div className="App">
+      <Helmet>
+        <title>TAXTIX - Permainan Tebak Pajak Setiap Hari</title>
+        <meta name="description" content="Mainkan TAXTIX - Permainan Kata tentang Pajak. Sebuah permainan yang menyenangkan dan edukatif untuk mempelajari istilah perpajakan sambil bersenang-senang!" />
+        <meta name="keywords" content="permainan kata pajak, edukasi pajak, wordle pajak, TAXTIX, permainan belajar pajak, permainan edukatif, pembelajaran keuangan" />
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Vito Atmo" />
+        <meta name="publisher" content="Atmo Intelligence" />
+        <html lang="id" />
+      </Helmet>
       <Header />
       <main className="main-body">
         <WordleGrid
